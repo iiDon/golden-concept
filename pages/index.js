836aed7fe352036c70e595db3cf4dd1d14745ede
Sultan <abu-farris@hotmail.com>
2022-10-27 08:25:47 +0300

    d

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import { Box, Image, Text, VStack } from "@chakra-ui/react";
+import Link from "next/link";
 
 export default function Home() {
   const images = [
@@ -6,16 +7,19 @@ export default function Home() {
       title: "Brand Digital",
       descreiption: "Standing out by simply saying less",
       url: "/Brand.jpg",
+      link: "/project/standing-out-by-simply-saying-less",
     },
     {
       title: "Graphic Design",
       descreiption: "Design for one of the major figures of fashion",
       url: "/Graphic.jpg",
+      link: "/project/design-for-one-of-the-major-figures-of-fashion",
     },
     {
       title: "Industrial Design",
       descreiption: "Better Outcomes for All",
       url: "/Industrial.jpg",
+      link: "/project/better-outcomes-for-all",
     },
   ];
 
@@ -42,8 +46,12 @@ export default function Home() {
 
         <Box>
           {images.map((image) => (
-            <>
-              <VStack justifyContent="center" alignItems="center">
+            <Link href={image.link}>
+              <VStack
+                justifyContent="center"
+                alignItems="center"
+                cursor="pointer"
+              >
                 <Text
                   textColor="#8A8A8A"
                   as="b"
@@ -54,7 +62,7 @@ export default function Home() {
                 <Text py={5}>{image.descreiption}</Text>
                 <Image pb={4} src={image.url} alt={image.title} />
               </VStack>
-            </>
+            </Link>
           ))}
         </Box>
       </Box>
